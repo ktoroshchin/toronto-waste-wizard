e0129ceac4b16077e3bbd5166b309fc19d7eebd4
Macbook <k.toroshchin@gmail.com>
2019-01-10 14:29:48 -0500

    Main Project Setup

---
+++ client/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          Hello World
+      </nav>
+    );
+  }
+}
+
+export default App;
